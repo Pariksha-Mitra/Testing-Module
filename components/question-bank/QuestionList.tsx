e26ae58740519e1e87cb4d96854be71e562e3388
Rajvardhan Desai <103950321+Rajvardhan-Desai@users.noreply.github.com>
2025-01-05 17:46:29 +0000

    Add QuestionCard and QuestionList components for question-bank

---
+++ components/question-bank/QuestionList.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { QuestionCard } from "./QuestionCard";
+import { QuestionListProps } from "@/utils/types";
+import Link from "next/link";
+
+export const QuestionList: React.FC<QuestionListProps> = ({
+  questions,
+  selectedQuestionIndex,
+  onQuestionSelect,
+  onDeleteQuestion,
+}) => {
+  const handleAddQuestion = () => {
+    console.log("Add question clicked");
+  };
+
+  return (
+    <div className="flex flex-col gap-3 text-2xl mr-3 p-2">
+      {/* Special card for adding a question */}
+      <Link href={"/create-test"}>
+        <QuestionCard
+          id="add-question"
+          questionNumber="+"
+          description="Add question"
+          isSelected={false}
+          onClick={handleAddQuestion}
+        />
+      </Link>
+      {/* Map over existing questions */}
+      {questions.map((question, index) => (
+        <QuestionCard
+          id={question.id}
+          key={question.id} // Ensure each question has a unique id
+          questionNumber={question.questionNumber}
+          description={question.description}
+          isSelected={selectedQuestionIndex === index}
+          onClick={() => onQuestionSelect(index)}
+          onDelete={() => onDeleteQuestion(index)}
+        />
+      ))}
+    </div>
+  );
+};
