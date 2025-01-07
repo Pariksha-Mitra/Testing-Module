a0605e4d1d428e0efad8187284c592a74c3006c0
Akhilesh26 <akhileshpimple3@gmail.com>
2025-01-07 19:05:24 +0530

    add delete logic

--- app/api/standard/route.ts
+++ app/api/standard/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import {Standard} from "@/server/models/questionsSchema";
 import connectDB from "@/server/utils/db";
+import { Chapter } from "@/server/models/questionsSchema";
+import { Exercise } from "@/server/models/questionsSchema";
+import { Question } from "@/server/models/questionsSchema";
 
 export async function GET() {
     try {
@@ -30,3 +33,44 @@ export async function POST(req: Request) {
     return NextResponse.json({error: "Failed to create standard" },{status: 500 });
   }
 }
+
+
+export async function DELETE(req: Request) {
+  try {
+    // Connect to the database
+    await connectDB();
+
+    const {searchParams} = new URL(req.url);
+    const standardId = searchParams.get("id");
+    
+    if (!standardId) {
+      return NextResponse.json({ error: "Id not Found" }, { status: 404 });
+    }
+
+
+    const chaptersToDelete = await Chapter.find({ standard: standardId });
+    const chapterIds = chaptersToDelete.map(chapter => chapter._id);
+
+    const exercisesToDelete = await Exercise.find({ chapter: { $in: chapterIds } });
+    const exerciseIds = exercisesToDelete.map(exercise => exercise._id);
+
+    await Question.deleteMany({ exercise: { $in: exerciseIds } });
+
+    await Exercise.deleteMany({ _id: { $in: exerciseIds } });
+
+    await Chapter.deleteMany({ _id: { $in: chapterIds } });
+
+    const deletedStandard = await Standard.findByIdAndDelete(standardId);
+
+    if (!deletedStandard) {
+      return NextResponse.json({ error: "Standard not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Standard and related data deleted successfully" }, { status: 200 });
+
+  } catch (error) {
+    console.error("Error deleting standard and related data:", error);
+    return NextResponse.json({ error: "Failed to delete standard and related data" }, { status: 500 });
+  }
+}
+
