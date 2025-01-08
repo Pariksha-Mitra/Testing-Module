ccdfc7613b4f3848d103c545e73b898ff976c218
Akhilesh26 <akhileshpimple3@gmail.com>
2025-01-08 17:00:07 +0530

    add swagger comments

--- app/api/exercises/[exerciseId]/route.ts
+++ app/api/exercises/[exerciseId]/route.ts
@@ -1,6 +1,56 @@
 import { NextResponse } from "next/server";
-import { Exercise } from "@/server/models/questionsSchema";
-import connectDB from "@/server/utils/db";
+import { Exercise } from "@/models/questionsSchema";
+import {connectDB} from "@/utils/db";
+
+/**
+ * @swagger
+ * paths:
+ *   /api/exercises/{exerciseId}:
+ *     get:
+ *       tags:
+ *         - Exercises
+ *       summary: "Get a single exercise by its ID"
+ *       description: "Retrieve an exercise by its ID along with related details such as chapter and questions."
+ *       parameters:
+ *         - name: "exerciseId"
+ *           in: "path"
+ *           required: true
+ *           description: "The ID of the exercise to retrieve."
+ *           schema:
+ *             type: "string"
+ *       responses:
+ *         200:
+ *           description: "Successfully retrieved the exercise."
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: "object"
+ *                 properties:
+ *                   singleExercise:
+ *                     $ref: "#/components/schemas/Exercise"
+ *         404:
+ *           description: "Exercise not found."
+ *         500:
+ *           description: "Failed to retrieve the exercise."
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Exercise:
+ *       type: "object"
+ *       properties:
+ *         _id:
+ *           type: "string"
+ *         title:
+ *           type: "string"
+ *         description:
+ *           type: "string"
+ *         chapter:
+ *           type: "string"
+ */
+
 
 export async function GET(req: Request, context: any) {
   try {
