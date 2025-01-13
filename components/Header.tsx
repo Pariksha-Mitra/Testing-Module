e81bc176de02bc60c52ba4c6adf886d001bf77c8
Rajvardhan Desai <103950321+Rajvardhan-Desai@users.noreply.github.com>
2025-01-13 11:15:25 +0000

    Update Header.tsx; font change

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,18 +4,22 @@ import Image from "next/image";
 
 export default function Header() {
   const { data: session } = useSession();
-
+  
+  console.log(session?.user);
   return (
-    <div className="bg-blue-500 text-white flex items-center justify-between p-4 rounded-lg shadow">
+    <div className="bg-[#6378FD] text-white flex items-center justify-between px-5 py-3 rounded-[20px] shadow">
       <div className="flex items-center space-x-4">
-        <div className="w-20 h-20 bg-white rounded-full"></div>
-        <div>
-          <h1 className="text-3xl font-bold rozha-one-bold">स्वागत आहे!</h1>
-          <p className="text-sm">{session?.user.username}, DETAILS</p>
+        <div className=" w-32 h-32 bg-white rounded-full"></div>
+        <div className="pl-10">
+          <h1 className="text-6xl font-bold rozha-one-regular">स्वागत आहे !</h1>
+          <p className="text-3xl laila-light">{session?.user.username}, DETAILS</p>
+          
         </div>
       </div>
-      <button onClick={() => signOut()}>
+      <button onClick={() => signOut()} >
         <Image src="/logout.png" alt="Logout" width={100} height={100} />
+        <div className="arya-bold mt-[-15px]">लॉगआउट</div>
+        
       </button>
     </div>
   );
