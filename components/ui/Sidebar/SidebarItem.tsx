3446508385b48ae256ab2310e5cb7732c0aab636
Rajvardhan-Desai <rajvardhandesai9999@gmail.com>
2025-01-18 15:38:50 +0530

    feat: add unsaved changes confirmation in SidebarItem and introduce isEditing state in QuestionsContext

--- components/ui/Sidebar/SidebarItem.tsx
+++ components/ui/Sidebar/SidebarItem.tsx
@@ -1,15 +1,28 @@
-"use client"
+"use client";
 import React from "react";
 import Link from "next/link";
 import { SidebarItemProps } from "@/utils/types";
 import { usePathname } from "next/navigation";
+import { useQuestions } from "@/context/QuestionsContext";
 
 const SidebarItem: React.FC<SidebarItemProps> = ({ href, ariaLabel, icon }) => {
   const pathname = usePathname();
+  const { isEditing } = useQuestions(); // Access isEditing from context
   const isActive = pathname === href;
 
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (isEditing) {
+      const confirmLeave = window.confirm(
+        "You have unsaved changes. Are you sure you want to leave this page?"
+      );
+      if (!confirmLeave) {
+        e.preventDefault(); // Prevent navigation
+      }
+    }
+  };
+
   return (
-    <Link href={href} aria-label={ariaLabel}>
+    <Link href={href} aria-label={ariaLabel} onClick={handleClick}>
       <div
         className={`w-14 h-14 ${
           isActive ? "bg-[#6378fd]" : "bg-white"
