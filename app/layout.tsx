cb54ea0666aac038346d44111235f02c46c54d5c
Rajvardhan-Desai <rajvardhandesai9999@gmail.com>
2025-01-25 17:34:39 +0530

    refactor: clean up layout component by removing unused QuestionsProvider and adjusting formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import AuthProvider from '@/utils/AuthProvider';
-import { Laila, Rozha_One, Arya } from 'next/font/google';
-import { ToastProvider } from '@/components/ui/ToastProvider';
-import { QuestionsProvider } from '@/context/QuestionsContext';
-import './globals.css';
+import AuthProvider from "@/utils/AuthProvider";
+import { Laila, Rozha_One, Arya } from "next/font/google";
+import { ToastProvider } from "@/components/ui/ToastProvider";
+import "./globals.css";
 import type { Metadata } from "next";
 // Extract font configurations
 const rozhaOne = Rozha_One({
@@ -24,7 +23,6 @@ const arya = Arya({
   variable: "--font-arya",
 });
 
-
 export const metadata: Metadata = {
   title: "Parikhsa Mitra",
   description: "A platform for marathi medium students",
@@ -40,10 +38,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${rozhaOne.variable} ${laila.variable} ${arya.variable} antialiased`}>
+        className={`${rozhaOne.variable} ${laila.variable} ${arya.variable} antialiased`}
+      >
         <AuthProvider>
-          <ToastProvider>
-            <QuestionsProvider>{children}</QuestionsProvider></ToastProvider>
+          <ToastProvider>{children}</ToastProvider>
         </AuthProvider>
       </body>
     </html>
